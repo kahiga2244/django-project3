a2dacf23b2cd6d982701e6e1bcbeee078384ad2a
kahiga2244 <kahigakamiru@gmail.com>
2021-04-13 11:13:48 +0300

    adding post/overview forms class models

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Profile, Neighbourhood
+from .models import Profile, Neighbourhood,Overview, Post
 from cloudinary.models import CloudinaryField
 
 class RegisterForm(UserCreationForm):
@@ -25,3 +25,12 @@ class NeighbourHoodForm(forms.ModelForm):
     class Meta:
         model = Neighbourhood
         fields = ('picture', 'name', 'location','health','supermarket','police')
+class OverviewForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        fields = ('name', 'email', 'description')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'post')
