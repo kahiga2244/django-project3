a406ab55be881383b49a3932486eb06e57285421
kahiga2244 <kahigakamiru@gmail.com>
2021-04-13 10:24:19 +0300

    adding neighborhood class models

--- hoodapp/models.py
+++ hoodapp/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
     name = models.CharField(max_length=80, blank=True)
     location = models.CharField(max_length=50, blank=True, null=True)
     profile_picture = CloudinaryField('image')
-    # neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='occupant', blank=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', on_delete=models.SET_NULL, null=True, related_name='occupant', blank=True)
 
     def __str__(self):
         return f'{self.user.username} profile'
@@ -24,3 +24,21 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+class Neighbourhood (models.Model):
+    
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    picture = CloudinaryField('image')
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='hood')
+    health = models.IntegerField(null=True, blank=True)
+    supermarket = models.IntegerField(null=True, blank=True)
+    police = models.IntegerField(null=True, blank=True)
+    def __str__(self):
+        return f'{self.name} Hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
