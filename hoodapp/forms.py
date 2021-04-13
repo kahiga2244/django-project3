0cad499690d9417c0e882b805a55655e7d06dc91
kahiga2244 <kahigakamiru@gmail.com>
2021-04-13 10:21:16 +0300

    adding a new neighborhood(hform

--- hoodapp/forms.py
+++ hoodapp/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-# from .models import Profile, Neighbourhood, Business, Post
-# from cloudinary.models import CloudinaryField
+from .models import Profile, Neighbourhood
+from cloudinary.models import CloudinaryField
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254)
@@ -10,3 +10,18 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
+
+class UpdateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=254)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    picture = CloudinaryField('image')
+
+    class Meta:
+        model = Neighbourhood
+        fields = ('picture', 'name', 'location','health','supermarket','police')
