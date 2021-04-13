a02e9c9ca7da5369a24ff856fa6e805fe3649415
kahiga2244 <kahigakamiru@gmail.com>
2021-04-13 11:50:56 +0300

    confirming on urls links to views

--- hoodapp/urls.py
+++ hoodapp/urls.py
@@ -6,5 +6,17 @@ urlpatterns = [
     path('',views.index,name = 'index'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path('profile/<username>/', views.profile, name='profile'),
+    path('profile/<username>/edit', views.edit_profile, name='edit'),
+    path('create-hoodz/', views.create_hoodz, name='create_hoodz'),
+    path('mahoodz/', views.mitaa, name='mahoodz'),
+    path('hoodz/<hoodz_id>', views.hoodz, name='hoodz'),
+    path('join-hoodz/<id>', views.join_hoodz, name='join_hoodz'),
+    path('leave-hoodz/<id>', views.leave_hoodz, name='leave_hoodz'),
+    path('occupants/<hoodz_id>', views.hoodz_occupants, name='occupants'),
+    path('business/<hoodz_id>', views.business, name='business'),
+    path('post/<hoodz_id>', views.post, name='post'),
+    path('search/', views.search, name='search'),
 
 ]
