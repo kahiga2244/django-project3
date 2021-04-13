b36d9169bdeea3ce20e77b7f4ee7ce7626d2136f
kahiga2244 <kahigakamiru@gmail.com>
2021-04-13 10:02:32 +0300

    adlets allow users to edit profile

--- hoodapp/views.py
+++ hoodapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Profile, User
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RegisterForm
+from .forms import RegisterForm,UpdateUserForm
 # Create your views here.
 @login_required(login_url='login')
 def index(request):
@@ -30,3 +30,21 @@ def register(request):
 @login_required(login_url='login')
 def profile(request, username):
     return render(request, 'profile.html')
+
+
+@login_required(login_url='login')
+def edit_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        user_form = UpdateUserForm(request.POST, instance=request.user)
+        prof_form = UpdateProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and prof_form.is_valid():
+            user_form.save()
+            prof_form.save()
+            return redirect('profile', user.username)
+    else:
+        user_form = UpdateUserForm(instance=request.user)
+        prof_form = UpdateProfileForm(instance=request.user.profile)
+    
+    return render(request, 'editprofile.html', {'user_form': user_form, 'prof_form': prof_form})
+
